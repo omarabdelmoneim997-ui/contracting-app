640a34b50381191394f9d82fd322b2fb3340bbf3
omarabdelmoneim997-ui <omarabdelmoneim997@gmail.com>
2026-08-20 23:19:48 +0300

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,1 +1,6 @@
+import { createClient } from "@supabase/supabase-js";
 
+const SUPABASE_URL = "https://pywkkzedqqgmaghdqxba.supabase.co";
+const SUPABASE_KEY = "sb_publishable_NdCF4PCw-OVWa0YwIfMVPA_y0IoJTLF";
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
